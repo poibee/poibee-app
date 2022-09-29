9e287c70ffb1414d4e613a49031af9dbecbdbcc0
poibee <info@poibee.org>
2022-09-30 00:09:11 +0200

    #28 POI List: Extracted services to filter and sort pois

---
+++ src/app/services/pois-filter.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {Poi} from "../data/poi";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PoisFilterService {
+
+  constructor() {
+  }
+
+  filterPois(pois: Poi[], filterValue: string): Poi[] {
+    const resultPois: Poi[] = [];
+    pois.forEach(poi => {
+      if (filterValue.length == 0 || (poi.name && poi.name.toLowerCase().includes(filterValue.toLowerCase()))) {
+        resultPois.push(poi);
+      }
+    });
+    return resultPois;
+  }
+}
