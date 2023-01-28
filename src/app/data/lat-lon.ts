92fadd9aefda3896daecadee4d86edffa2480bb7
poibee <info@poibee.org>
2023-01-28 01:42:05 +0100

    #68 extract query parameter from url and start searching

--- src/app/data/lat-lon.ts
+++ src/app/data/lat-lon.ts
@@ -1,6 +1,11 @@
 export class LatLon {
   constructor(public readonly lat: number, public readonly lon: number) {}
 
+  static ofPosition(position: string): LatLon {
+    const positionParts = position.split(',');
+    return new LatLon(Number.parseInt(positionParts[0]), Number.parseInt(positionParts[1]));
+  }
+
   asLatLng() {
     return {
       lat: this.lat,
