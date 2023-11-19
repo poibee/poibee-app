d116f94102e38d8301eacdae7ea94440c50652e3
Poibee <info@poibee.org>
2023-11-20 00:00:10 +0100

    #87 POI-Map: created cypress page object

--- cypress/e2e/discover-map-spec.cy.ts
+++ cypress/e2e/discover-map-spec.cy.ts
@@ -1,64 +1,68 @@
+import DiscoverPage from "../pages/discover-page";
+
 describe('Discover page shows map', () => {
 
+  const discoverPage = new DiscoverPage()
+
   beforeEach(() => {
-    Cypress.config('defaultCommandTimeout', 60000);
+    Cypress.config('defaultCommandTimeout', 60000)
 
     cy.viewport('iphone-x')
 
-    cy.visit('/discover')
+    discoverPage.open()
   });
 
   describe('with position control', () => {
 
     it('"geocode" should move map to searched "Hamburg"', () => {
-      cy.get('.leaflet-pane.leaflet-marker-pane .leaflet-marker-icon').should('have.attr', 'data-cy-marker-lat', '52.908')
-      cy.get('.leaflet-pane.leaflet-marker-pane .leaflet-marker-icon').should('have.attr', 'data-cy-marker-lon', '8.588')
+      discoverPage.mapPositionMarker().should('have.attr', 'data-cy-marker-lat', '52.908')
+      discoverPage.mapPositionMarker().should('have.attr', 'data-cy-marker-lon', '8.588')
 
-      cy.get('[data-cy=componentDiscoverMap] .leaflet-control [aria-label="Initiate a new search"]').click()
-      cy.get('[data-cy=componentDiscoverMap] .leaflet-control-geocoder input').type('Hamburg')
+      discoverPage.mapControlPositionByGeocodeSearchOpen().click()
+      discoverPage.mapControlPositionByGeocodeSearchTextfield().type('Hamburg')
 
       // ignore search, because it's only working locally not at GitHub-CI
-      // cy.get('[data-cy=componentDiscoverMap] .leaflet-control-geocoder input').type('{enter}')
+      // discoverPage.mapControlPositionByGeocodeSearchTextfield().type('{enter}')
 
-      // cy.get('.leaflet-pane.leaflet-marker-pane .leaflet-marker-icon').should('have.attr', 'data-cy-marker-lat', '53.550341')
-      // cy.get('.leaflet-pane.leaflet-marker-pane .leaflet-marker-icon').should('have.attr', 'data-cy-marker-lon', '10.000654')
+      // discoverPage.mapPositionMarker().should('have.attr', 'data-cy-marker-lat', '53.550341')
+      // discoverPage.mapPositionMarker().should('have.attr', 'data-cy-marker-lon', '10.000654')
     })
 
     it('"locate" should move map and position to current device position', () => {
-      cy.get('.leaflet-pane.leaflet-marker-pane .leaflet-marker-icon').should('have.attr', 'data-cy-marker-lat', '52.908')
-      cy.get('.leaflet-pane.leaflet-marker-pane .leaflet-marker-icon').should('have.attr', 'data-cy-marker-lon', '8.588')
+      discoverPage.mapPositionMarker().should('have.attr', 'data-cy-marker-lat', '52.908')
+      discoverPage.mapPositionMarker().should('have.attr', 'data-cy-marker-lon', '8.588')
 
-      cy.get('[data-cy=componentDiscoverMap] .leaflet-control [title="Standort lokalisieren"]').click()
+      discoverPage.mapControlPositionByDeviceLocation().click()
 
       // ignore handling of browser popup at the moment
     })
 
     it('"map center" should move position to map center', () => {
-      cy.get('.leaflet-pane.leaflet-marker-pane .leaflet-marker-icon').should('have.attr', 'data-cy-marker-lat', '52.908')
-      cy.get('.leaflet-pane.leaflet-marker-pane .leaflet-marker-icon').should('have.attr', 'data-cy-marker-lon', '8.588')
+      discoverPage.mapPositionMarker().should('have.attr', 'data-cy-marker-lat', '52.908')
+      discoverPage.mapPositionMarker().should('have.attr', 'data-cy-marker-lon', '8.588')
 
       // ignore drag and drop of the map at the moment
 
-      cy.get('[data-cy=componentDiscoverMap] .leaflet-control [title="Kartenmitte als Standort"]').click()
+      discoverPage.mapControlPositionByMapCenter().click()
     })
   });
 
   describe('with zoom control', () => {
 
     it('"Zoom in"', () => {
-      cy.get('[data-cy=componentDiscoverMap] .map').should('have.attr', 'data-cy-map-zoom', '13')
+      discoverPage.mapLeafletNative().should('have.attr', 'data-cy-map-zoom', '13')
 
-      cy.get('[data-cy=componentDiscoverMap] .leaflet-control [title="Zoom in"]').click()
+      discoverPage.mapControlZoomIn().click()
 
-      cy.get('[data-cy=componentDiscoverMap] .map').should('have.attr', 'data-cy-map-zoom', '14')
+      discoverPage.mapLeafletNative().should('have.attr', 'data-cy-map-zoom', '14')
     })
 
     it('"Zoom out"', () => {
-      cy.get('[data-cy=componentDiscoverMap] .map').should('have.attr', 'data-cy-map-zoom', '13')
+      discoverPage.mapLeafletNative().should('have.attr', 'data-cy-map-zoom', '13')
 
-      cy.get('[data-cy=componentDiscoverMap] .leaflet-control [title="Zoom out"]').click()
+      discoverPage.mapControlZoomOut().click()
 
-      cy.get('[data-cy=componentDiscoverMap] .map').should('have.attr', 'data-cy-map-zoom', '12')
+      discoverPage.mapLeafletNative().should('have.attr', 'data-cy-map-zoom', '12')
     })
   });
 
